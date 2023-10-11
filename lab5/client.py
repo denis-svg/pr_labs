2420667f4ef033b85530ff838de1ac9cdfd2a759
Prodan Denis <64483300+denis-svg@users.noreply.github.com>
2023-10-11 14:16:26 +0300

    Add files via upload

---
+++ lab5/client.py
@@ -0,0 +1,107 @@
+#client.py
+
+import socket
+import threading
+import json
+import os
+import base64
+
+# Server configuration
+HOST = '127.0.0.1'  # Server's IP address
+PORT = 8080       # Server's port
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Connect to the server
+client_socket.connect((HOST, PORT))
+print(f"Connected to {HOST}:{PORT}")
+
+# Function to receive and display messages
+def receive_messages():
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        if not message:
+            break  # Exit the loop when the server disconnects
+        message = message.replace("'", '"')
+        message = json.loads(message)
+        if message['type'] == "message":
+            print(f"\n{message['payload']['room']}/{message['payload']['name']}:{message['payload']['text']}")
+
+        elif message['type'] == "image":
+            filename = message["payload"]["filename"]
+            filedata = message["payload"]["filedata"]
+
+            # Save the received file
+            with open(message['payload']['name'] + filename, "wb") as file:
+                file.write(base64.b64decode(filedata))
+                
+            print(f"\n{message['payload']['room']}/{message['payload']['name']} sent a file: {filename}")
+
+def get_general_info():
+    name = input("Enter your name:")
+    room = input("Enter a room:")
+    
+    return {
+            "name":name,
+            "room":room
+            }
+
+def init(gen_info):
+    receive_thread = threading.Thread(target=receive_messages)
+    receive_thread.daemon = True  # Thread will exit when the main program exits
+    receive_thread.start()
+    json_sent = {
+        "type": "connect_ack",
+        "payload": {
+            "room":gen_info["room"],
+            "name":gen_info["name"]
+        }
+    }
+
+    client_socket.send(str(json_sent).encode('utf-8'))
+
+def has_extension(filename, extension):
+    return filename.endswith(extension) and " " not in filename
+
+gen_info = get_general_info()
+init(gen_info)
+
+while True:
+    message = input(f"{gen_info['room']}/{gen_info['name']}:")
+
+    if message.lower() == 'exit':
+        break
+
+    if has_extension(message, ".txt") or has_extension(message, ".png") or has_extension(message, ".py"):
+        file = open(message, "rb")
+        file_size = os.path.getsize(message)
+        data = file.read()
+        file.close()
+        print("Client", data, base64.b64encode(data).decode('ascii'))
+
+        json_sent = {
+            "type": "image",
+            "payload":{
+                "filename":message,
+                "filesize":file_size,
+                "filedata":base64.b64encode(data).decode('ascii'),
+                "room":gen_info["room"],
+                "name":gen_info["name"],
+            }
+        }
+    else:
+        json_sent = {
+            "type": "message",
+            "payload":{
+                "text":message,
+                "room":gen_info["room"],
+                "name":gen_info["name"],
+            }
+        }
+
+    client_socket.send(str(json_sent).encode('utf-8'))
+ 
+# Close the client socket when done
+client_socket.close()
---
+++ lab5/server.py
@@ -0,0 +1,79 @@
+#server.py
+import socket
+import threading
+import json
+import base64
+
+# ... (Code from Step 1)
+# Server configuration
+HOST = '127.0.0.1'  # Loopback address for localhost
+PORT = 8080       # Port to listen on
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Bind the socket to the specified address and port
+server_socket.bind((HOST, PORT))
+# Listen for incoming connections
+server_socket.listen()
+print(f"Server is listening on {HOST}:{PORT}")
+
+# Function to handle a client's messages
+def handle_client(client_socket, client_address, client_id):
+    print(f"Accepted connection from {client_address}")
+    while True:
+        message = client_socket.recv(1024).decode('utf-8')
+        if not message:
+            break  # Exit the loop when the client disconnects
+        
+        print(f"Received from {client_address}: {message}")
+        message = message.replace("'", '"')
+        
+        message = json.loads(message)
+        
+        if message['type'] == "connect_ack":
+            clients[client_id]["name"] = message["payload"]["name"]
+            clients[client_id]["room"] = message["payload"]["room"]
+        elif message['type'] == "message":
+            text = message["payload"]["text"]
+            # Broadcast the message to all clients
+            for sk in sockets:
+                for c in clients.keys():
+                    if clients[c]["socket"] == sk:
+                        if clients[c]["room"] == message["payload"]["room"]:
+                            if sk != client_socket:
+                                sk.send(str(message).encode('utf-8'))
+        elif message['type'] == "image":
+            # Received an image/file from the client
+            filename = message["payload"]["filename"]
+            filedata = message["payload"]["filedata"]
+
+            with open("server_" + filename, "wb") as file:
+                file.write(base64.b64decode(filedata))
+
+            # Broadcast the file to clients in the same room
+            for sk in sockets:
+                for c in clients.keys():
+                    if clients[c]["socket"] == sk:
+                        if clients[c]["room"] == message["payload"]["room"]:
+                            if sk != client_socket:
+                                sk.send(str(message).encode('utf-8'))
+    sockets.remove(client_socket)
+    del clients[client_id]
+    client_socket.close()
+
+clients = {}
+cid = 0
+sockets = []
+while True:
+    client_socket, client_address = server_socket.accept()
+    sockets.append(client_socket)
+    clients[f"user{cid}"] = {
+                        "socket": client_socket,
+                        "name":None,
+                        "room":None
+                        }
+    # Start a thread to handle the client
+    client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address, f"user{cid}"))
+    client_thread.start()
+    cid += 1
